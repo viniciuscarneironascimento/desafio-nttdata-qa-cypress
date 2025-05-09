aba9461046bcaf727da98b97a7e811eb466fa08c
Vinicius <contato.vinicius@gmail.com>
2025-05-09 10:48:57 -0300

    Inclusão workflow CI Github Actions

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://front.serverest.dev',
+    env: {
+      apiBaseUrl: 'https://serverest.dev'
+    },
+    chromeWebSecurity: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    },
-    chromeWebSecurity: false
-  },
+    }
+  }
 });
+
